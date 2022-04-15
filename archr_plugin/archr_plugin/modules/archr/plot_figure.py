e21439c96ccd85cdf65209f7e0bb61a3318f0eb6
Kai Hu <>
2022-04-15 10:25:05 -0400

    minor

--- archr_plugin/archr_plugin/modules/archr/plot_figure.py
+++ archr_plugin/archr_plugin/modules/archr/plot_figure.py
@@ -17,11 +17,14 @@ def plot_figure(self, dir_n, endswith = ".jpg", img_width = "32%", name = "To be
         print("TEST")
         print(f['fn'], f['root'])
         if dir_n in f['root']:
+            print("IN\n")
             if f['fn'].endswith(endswith):
                 plot_path = os.path.join(f['root'], f['fn'])
                 self.archr[plot_path] = f
                 plot_path_all = plot_path_all + plot_path + "<br>"
                 plot = plot + "<figure style=\"border:solid 1px black; display:inline-block; width:" + img_width + "; margin:0.5%\">" + "<figcaption style=\"text-align: center; height: 40px; padding: 3px\">" + f['s_name'] + "</figcaption>" + " <img width=100% src=\"" + plot_path + "\">" + " </figure>"
+        else:
+            print("NOT IN\n")
     if plot == "":
         # deal with amulet filter module:
         if dir_n == "amulet_filter_doublets/":
