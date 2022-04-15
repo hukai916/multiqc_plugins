24f01cc1a2435a928af938f17a2261a596ae0f8e
Kai Hu <>
2022-04-15 09:47:40 -0400

    minor

--- get_valid_barcode_plugin/get_valid_barcode_plugin/custom_code.py
+++ get_valid_barcode_plugin/get_valid_barcode_plugin/custom_code.py
@@ -37,6 +37,8 @@ def get_valid_barcode_plugin_execution_start():
         print("not in")
         config.update_dict( config.sp, { 'get_valid_barcode/jpeg': { 'fn': 'pdf' } } )
         print("update done")
+        print(config.sp)
+
 
     # Some additional filename cleaning
     config.fn_clean_exts.extend([
