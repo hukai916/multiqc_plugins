c7611fe982a36001d619e27a44246bdb7746e5b1
Kai Hu <>
2022-04-15 10:46:42 -0400

    minor

--- get_valid_barcode_plugin/get_valid_barcode_plugin/modules/get_valid_barcode/get_valid_barcode.py
+++ get_valid_barcode_plugin/get_valid_barcode_plugin/modules/get_valid_barcode/get_valid_barcode.py
@@ -35,7 +35,7 @@ class MultiqcModule(BaseMultiqcModule):
         )
         self.get_valid_barcode = dict()
         print("test before")
-        plot_figure(self, dir_n = "get_valid_barcode", endswith = "*.jpg", img_width = "32%", name = "Valid barcodes", description = "Valid cell barcodes are inferred with 'inflection point' method as the barcode count cutoff is set to the inflection point in the plot.", helptext = "Cell barcodes in the top-left corner of the plot are valid.")
+        plot_figure(self, dir_n = "get_valid_barcode", endswith = "*.jpeg", img_width = "32%", name = "Valid barcodes", description = "Valid cell barcodes are inferred with 'inflection point' method as the barcode count cutoff is set to the inflection point in the plot.", helptext = "Cell barcodes in the top-left corner of the plot are valid.")
 
         # Filter out samples matching ignored sample names
         self.get_valid_barcode = self.ignore_samples(self.get_valid_barcode)
